cd22464e560096fd981c165248149b97410bc31c
AniForLearning <mnatsakanyanani409@gmail.com>
2025-03-02 22:19:14 +0400

    Update test_login.py

--- test_login.py
+++ test_login.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium.webdriver.common.by import By
 #from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
@@ -5,16 +7,13 @@ import pytest
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from locators.login_locators import  Login_path, Base_URL
 
-@pytest.fixture
-def driver():
-    driver = webdriver.Chrome()
-    driver.implicitly_wait(5)
-    yield driver
-    driver.quit()
+
+
 
 def test_open_browser(driver):
-    driver.get('https://practicetestautomation.com/practice-test-login/')
+    driver.get(Base_URL + Login_path)
     username = driver.find_element(by=By.XPATH, value='//*[@id="username"]')
     password = driver.find_element(by=By.ID, value='password')
     username.click()
@@ -23,7 +22,7 @@ def test_open_browser(driver):
     WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, "password")))
 
 def test_valid_login(driver):
-    driver.get('https://practicetestautomation.com/practice-test-login/')
+    driver.get(Base_URL + Login_path)
     driver.find_element(by=By.ID, value='username').send_keys('student')
     driver.find_element(by=By.ID, value='password').send_keys('Password123')
     driver.find_element(by=By.ID, value='submit').click()
@@ -31,16 +30,19 @@ def test_valid_login(driver):
     assert "logged-in-successfully" in driver.current_url
 
 def test_invalid_username(driver):
-    driver.get('https://practicetestautomation.com/practice-test-login/')
+    driver.get( Base_URL + Login_path)
     driver.find_element(by=By.ID, value='username').send_keys('not student')
     driver.find_element(by=By.ID, value='password').send_keys('Password123')
     driver.find_element(by=By.ID, value='submit').click()
     error = driver.find_element(by=By.ID, value='error').text
+    print(error)
+    time.sleep(5)
+
     WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.ID, 'error')))
     assert "Your username is invalid!" in error
 
 def test_invalid_password(driver):
-    driver.get('https://practicetestautomation.com/practice-test-login/')
+    driver.get(Base_URL + Login_path)
     driver.find_element(by=By.ID, value='username').send_keys('student')
     driver.find_element(by=By.ID, value='password').send_keys('notPassword123')
     driver.find_element(by=By.ID, value='submit').click()
@@ -49,8 +51,10 @@ def test_invalid_password(driver):
     assert "Your password is invalid!" in error
 
 def test_empty_fields(driver):
-    driver.get('https://practicetestautomation.com/practice-test-login/')
+    driver.get(Base_URL + Login_path)
     driver.find_element(by=By.ID, value='submit').click()
     error = driver.find_element(by=By.ID, value='error').text
+
     WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.ID, 'error')))
     assert "Your username is invalid!" in error
+
